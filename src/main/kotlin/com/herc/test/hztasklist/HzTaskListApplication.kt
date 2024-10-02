74fd990dac0ea856e0eec0f9aaead2000fc4d6b2
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2024-10-02 11:17:00 +0300

    Initial commit

---
+++ src/main/kotlin/com/herc/test/hztasklist/HzTaskListApplication.kt
@@ -0,0 +1,11 @@
+package com.herc.test.hztasklist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HzTaskListApplication
+
+fun main(args: Array<String>) {
+	runApplication<HzTaskListApplication>(*args)
+}
